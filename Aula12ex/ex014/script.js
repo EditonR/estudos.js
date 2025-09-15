b4a1c0fd41a10e704c3337266a2a6c0161a96b8e
EditonR <editonrenne1997@hotmail.com>
2025-09-15 16:51:45 -0300

    Acrescentei uma mudança - Mudar o título do h1 de acordo com a função.

--- Aula12ex/ex014/script.js
+++ Aula12ex/ex014/script.js
@@ -1,8 +1,11 @@
 function carregar() {
   var msg = document.getElementById('msg')
   var img = document.getElementById('imagem')
+  var titulo = document.getElementById('titulo') // Irá pegar o h1
   var data = new Date()
   var hora = data.getHours()
+
+  
   
   
 
@@ -11,14 +14,18 @@ function carregar() {
     //Bom dia!
     img.src = 'fotomanha.jpg'
     document.body.style.background = '#ede8ddff' // Para mudar a cor de fundo com JavaScript
-  } else if ( hora >= 12 && hora <= 18) {
+    titulo.innerText = 'Hora da Manhã!'
+    
+  } else if ( hora >= 12 && hora <= 17) {
     //Boa tarde!
     img.src = 'fototarde.jpg'
     document.body.style.background = '#f39b79ff'
+    titulo.innerText = 'Hora da tarde!'
 
   } else {
     //Boa noite!
     img.src = 'fotonoite.jpg'
     document.body.style.background = '#0a0a17ff'
+    titulo.innerText = 'Hora da Noite!'
   }
 }
