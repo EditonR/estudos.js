ae7b9106cc34954bc5e7ba8d0872e71a87801513
EditonR <editonrenne1997@hotmail.com>
2025-11-10 16:16:19 -0300

    Estrutura de controle com for + usando Debug

---
+++ Aula14/ambiente.js
@@ -0,0 +1,21 @@
+/*
+var c = 1
+while (c <= 5 ) {
+  console.log(c)
+  c++
+}
+
+
+Ou 
+var c = 1
+do {
+  console.log(c)
+  c++
+} while (c <= 5 )
+
+  */
+ //---------------------------------------
+
+ for ( var c = 1; c <=13;c++){
+  console.log(c)
+ }
