5f7f8e498036b03adac19c08053760877616b7ad
EditonR <editonrenne1997@hotmail.com>
2025-11-10 15:25:44 -0300

    Repetições_Loops

---
+++ Aula13/ambiente.js
@@ -0,0 +1,19 @@
+var c = 1
+while (c <= 6 ){
+  console.log('Tudo bem?')
+  c++ // É um incremento igual na aula que utilizamos c = c + 1
+}
+
+
+
+
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?') */
