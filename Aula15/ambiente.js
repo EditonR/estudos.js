b07d0a98503031ec0bbc5da28b5074964ca4d215
EditonR <editonrenne1997@hotmail.com>
2025-11-24 14:31:12 -0300

    Aula 15 - Vetores - Variáveis compostas.

---
+++ Aula15/ambiente.js
@@ -0,0 +1,16 @@
+let num = [5,8,2,9,3]
+num.sort() // Ele arruma em ordem crescente
+num.push(1) //Ele adiciona um número no final array
+
+/*
+num.pop() // Remove o último elemento
+num.unshift('1') // Adiciona no começo do array
+num.shift() //Remove o primeiro elemento
+
+*/
+console.log(`Nosso vetor é o ${num}`)
+console.log(`O vetor tem ${num.length} posições`) // Ver a quantidade de elementos no vetor(array)
+console.log(`O primeiro valor do vetor é ${num[0]}`)//Seleciona o primeiro valor do vetor
+
+
+
